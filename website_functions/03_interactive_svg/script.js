542ecf7bdcc45dd05e93cce14e8626f4c98a5b0a
Henrike <henrike.wiemker@t-online.de>
2022-08-16 15:58:42 +0200

    #34 replace filter dropdown with searchable datalist

--- website_functions/03_interactive_svg/script.js
+++ website_functions/03_interactive_svg/script.js
@@ -1,64 +1,12 @@
 let metadata = [];
-var list = document.getElementById("genes-list");
-var genesList = ["gene_1", "gene_2", "gene_3", "gene_4", "gene_5", "gene_6"];
-genesList.forEach(function (item) {
-  var option = document.createElement("option");
-  option.value = item;
-  list.appendChild(option);
-});
 
-const loadData = async () => {
-  // read the hdf5 file as ArrayBuffer
-  const res = await fetch(
-    "https://export.uppmax.uu.se/snic2022-23-113/hdca_webdev/CCF/CCF_data.h5"
-  );
-  const arrayBuffer = await res.arrayBuffer();
-
-  // read as hdf5 file
-  const f = new hdf5.File(arrayBuffer);
-
-  // To check out the file structure of f, use f.keys
-  console.log(f.keys);
-  // will return ["matrix"]. This is the top level directory
-
-  // To check for deeper structure, use combination of .get and .keys
-  console.log(f.get("matrix").keys);
-  // will return ["barcodes", "data", "features", "indices", "indptr", "shape"]
-
-  // create variables containing the CCS notation arrays of the matrix
-  // x are the values, i the indices, p the indexpointer
-  const mx = f.get("matrix/data").value;
-  const mi = f.get("matrix/indices").value;
-  const mp = f.get("matrix/indptr").value;
-
-  console.log("mx", mx);
-  console.log("mi", mi);
-  console.log("mp", mp);
-
-  // check the dimensions of the matrix.
-  // will return an array with 2 values: [number_of_rows, number_of_columns]
-  const shape = f.get("matrix/shape").value;
-  console.log(shape);
-
-  Papa.parse(
-    "https://export.uppmax.uu.se/snic2022-23-113/hdca_webdev/CCF/CCF_meta.csv",
-    {
-      download: true,
-      complete: (results) => {
-        console.log(results);
-        metadata = results.data;
-        // Check the current value of the filter type dropdown to create right set of options
-        const filterType = document.getElementById("selectType").value;
-        switchType(filterType);
-      },
-    }
-  );
-};
-
-const createOptions = (columnNames) => {
-  document.getElementById("dropdown").innerHTML = "";
-  columnNames.map((name) => {
-    document.getElementById("dropdown").innerHTML += `<option>${name}</option>`;
+const createOptions = (optionArray) => {
+  const list = document.getElementById("filter-list");
+  list.innerHTML = "";
+  optionArray.forEach((item) => {
+    const option = document.createElement("option");
+    option.value = item;
+    list.appendChild(option);
   });
 };
 
@@ -93,7 +41,9 @@ const generateSeqLegend = (min, max) => {
   `;
 };
 
-const addColor = (columnName) => {
+// Function to add color to the svg based on a chosen filter.
+// columnName: string. The chosen filter.
+const showMetadata = (columnName) => {
   // The legend object will be populated with key value pairs
   // for each unique value we find in this column.
   // The unique value from the column will become a key in the object and
@@ -148,6 +98,26 @@ const addColor = (columnName) => {
   }
 };
 
+const showGenes = (gene) => {
+  // This will later create some visualization for genes.
+  // For now, just do some test stuff.
+  console.log("showing gene ", gene);
+};
+
+// Function to visualize a filter by adding color to the svg
+// Acts as a forwarding function that calls another function depending on the filter type.
+// That second function takes care of the actual coloring.
+// filter: string. A metadata column name or a gene name
+const visualize = (filter) => {
+  // Check if we are showing genes or metadata
+  const filterType = document.getElementById("selectType");
+  if (filterType === "metadata") {
+    showMetadata(filter);
+  } else {
+    showGenes(filter);
+  }
+};
+
 // Function to get a array of only numeric values
 // The data is the metadata provided in the project
 // The function applies to inflammation_level column values
@@ -192,8 +162,64 @@ const switchType = (type) => {
   if (type === "metadata") {
     createOptions(metadata[0]);
   } else {
-    createOptions(["eins", "zwei", "drei"]);
+    const genesList = [
+      "gene_1",
+      "gene_2",
+      "gene_3",
+      "gene_4",
+      "gene_5",
+      "gene_6",
+    ];
+    createOptions(genesList);
   }
+};
+
+const loadData = async () => {
+  // read the hdf5 file as ArrayBuffer
+  const res = await fetch(
+    "https://export.uppmax.uu.se/snic2022-23-113/hdca_webdev/CCF/CCF_data.h5"
+  );
+  const arrayBuffer = await res.arrayBuffer();
+
+  // read as hdf5 file
+  const f = new hdf5.File(arrayBuffer);
+
+  // To check out the file structure of f, use f.keys
+  console.log(f.keys);
+  // will return ["matrix"]. This is the top level directory
+
+  // To check for deeper structure, use combination of .get and .keys
+  console.log(f.get("matrix").keys);
+  // will return ["barcodes", "data", "features", "indices", "indptr", "shape"]
+
+  // create variables containing the CCS notation arrays of the matrix
+  // x are the values, i the indices, p the indexpointer
+  const mx = f.get("matrix/data").value;
+  const mi = f.get("matrix/indices").value;
+  const mp = f.get("matrix/indptr").value;
+
+  console.log("mx", mx);
+  console.log("mi", mi);
+  console.log("mp", mp);
+
+  // check the dimensions of the matrix.
+  // will return an array with 2 values: [number_of_rows, number_of_columns]
+  const shape = f.get("matrix/shape").value;
+  console.log(shape);
+
+  Papa.parse(
+    "https://export.uppmax.uu.se/snic2022-23-113/hdca_webdev/CCF/CCF_meta.csv",
+    {
+      download: true,
+      complete: (results) => {
+        console.log(results);
+        metadata = results.data;
+        // Check the current value of the filter type dropdown to create right set of options
+        const filterType = document.getElementById("selectType").value;
+        switchType(filterType);
+      },
+    }
+  );
 };
 
 window.onload = loadData();
